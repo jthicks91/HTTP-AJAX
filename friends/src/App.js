b2f461edcddbe4809ffd14675ddca02a2af169c2
jthicks91 <jordanthicks91@gmail.com>
2019-04-02 19:56:14 -0700

    rendered list of friends to screen. moving on to other steps of project for push.

---
+++ friends/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import { Route } from "react-router-dom";
+// import axios from "axios";
+import "./App.css";
+import Friends from "/Users/jordanhicks/Desktop/Projects/HTTP-AJAX/friends/src/Friends.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Friends />
+      </div>
+    );
+  }
+}
+export default App;
